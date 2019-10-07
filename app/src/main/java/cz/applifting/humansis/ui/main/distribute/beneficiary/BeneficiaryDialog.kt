6e52e2e377f69fbdeb9038e778673b0c39ad2b30
Václav Legát <vaclav.legat@applifting.cz>
2019-10-07 15:32:54 +0200

    Fix save only distributed not scanned

--- app/src/main/java/cz/applifting/humansis/ui/main/distribute/beneficiary/BeneficiaryDialog.kt
+++ app/src/main/java/cz/applifting/humansis/ui/main/distribute/beneficiary/BeneficiaryDialog.kt
@@ -1,6 +1,7 @@
 package cz.applifting.humansis.ui.main.distribute.beneficiary
 
 import android.Manifest
+import android.app.Dialog
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -18,7 +19,6 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.Result
-import cz.applifting.humansis.R
 import cz.applifting.humansis.extensions.visible
 import cz.applifting.humansis.ui.App
 import cz.applifting.humansis.ui.HumansisActivity
@@ -48,7 +48,15 @@ class BeneficiaryDialog : DialogFragment(), ZXingScannerView.ResultHandler {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NORMAL, R.style.FullscreenDialog)
+        setStyle(STYLE_NORMAL, cz.applifting.humansis.R.style.FullscreenDialog)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return object : Dialog(activity!!, theme) {
+            override fun onBackPressed() {
+                handleBackPressed()
+            }
+        }
     }
 
     override fun onCreateView(
@@ -56,19 +64,21 @@ class BeneficiaryDialog : DialogFragment(), ZXingScannerView.ResultHandler {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_beneficiary, container, false)
+        val view = inflater.inflate(cz.applifting.humansis.R.layout.fragment_beneficiary, container, false)
         (activity?.application as App).appComponent.inject(this)
         sharedViewModel = ViewModelProviders.of(activity as HumansisActivity, viewModelFactory)[SharedViewModel::class.java]
 
         view.apply {
-            btn_close.setOnClickListener { dismiss() }
+            btn_close.setOnClickListener {
+                handleBackPressed()
+            }
             tv_booklet.visible(args.isQRVoucher)
         }
 
         viewModel.beneficiaryLD.observe(viewLifecycleOwner, Observer {
             // Views
             view.apply {
-                tv_status.setValue(getString(if (it.distributed) R.string.distributed else R.string.not_distributed))
+                tv_status.setValue(getString(if (it.distributed) cz.applifting.humansis.R.string.distributed else cz.applifting.humansis.R.string.not_distributed))
                 tv_status.setStatus(it.distributed)
                 tv_beneficiary.setValue("${it.givenName} ${it.familyName}")
                 tv_distribution.setValue(args.distributionName)
@@ -76,14 +86,14 @@ class BeneficiaryDialog : DialogFragment(), ZXingScannerView.ResultHandler {
 
                 if (it.distributed) {
                     if (it.edited) {
-                        btn_action.text = context.getString(R.string.revert)
-                        btn_action.background = context.getDrawable(R.drawable.background_revert_btn)
+                        btn_action.text = context.getString(cz.applifting.humansis.R.string.revert)
+                        btn_action.background = context.getDrawable(cz.applifting.humansis.R.drawable.background_revert_btn)
                     } else {
                         btn_action.visible(false)
                     }
                 } else {
-                    btn_action.text = context.getString(R.string.confirm_distribution)
-                    btn_action.background = context.getDrawable(R.drawable.background_confirm_btn)
+                    btn_action.text = context.getString(cz.applifting.humansis.R.string.confirm_distribution)
+                    btn_action.background = context.getDrawable(cz.applifting.humansis.R.drawable.background_confirm_btn)
                 }
 
                 if (args.isQRVoucher) {
@@ -179,6 +189,16 @@ class BeneficiaryDialog : DialogFragment(), ZXingScannerView.ResultHandler {
         }
     }
 
+    private fun handleBackPressed() {
+        viewModel.beneficiaryLD.value?.let {
+            if (viewModel.distributed == null && !it.distributed && args.isQRVoucher) {
+                viewModel.editBeneficiary(false, args.beneficiaryId, null)
+            } else {
+                dismiss()
+            }
+        }
+    }
+
     private fun requestCameraPermission() {
         requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
     }
@@ -193,10 +213,10 @@ class BeneficiaryDialog : DialogFragment(), ZXingScannerView.ResultHandler {
 
     private fun showExplanationDialog() {
         val builder = AlertDialog.Builder(activity as HumansisActivity)
-        builder.setTitle(getString(R.string.camera_permission_dialog_title))
-        builder.setMessage(getString(R.string.camera_permission_dialog_message))
-        builder.setPositiveButton(getString(R.string.camera_permission_dialog_positive_btn_label)) { _, _ -> requestCameraPermission() }
-        builder.setNegativeButton(getString(R.string.camera_permission_dialog_negative_btn_label)) { _, _ -> findNavController().navigateUp() }
+        builder.setTitle(getString(cz.applifting.humansis.R.string.camera_permission_dialog_title))
+        builder.setMessage(getString(cz.applifting.humansis.R.string.camera_permission_dialog_message))
+        builder.setPositiveButton(getString(cz.applifting.humansis.R.string.camera_permission_dialog_positive_btn_label)) { _, _ -> requestCameraPermission() }
+        builder.setNegativeButton(getString(cz.applifting.humansis.R.string.camera_permission_dialog_negative_btn_label)) { _, _ -> findNavController().navigateUp() }
         val dialog = builder.create()
         dialog.show()
     }
