1632416f63f81d7b0556c8520b56986473e1caa8
Václav Legát <vaclav.legat@applifting.cz>
2019-11-26 13:45:51 +0100

    Add missing commodity types

--- app/src/main/java/cz/applifting/humansis/model/CommodityType.kt
+++ app/src/main/java/cz/applifting/humansis/model/CommodityType.kt
@@ -13,5 +13,11 @@ enum class CommodityType(val drawableResId: Int) {
     @SerializedName("Paper Voucher") PAPER_VOUCHER(R.drawable.ic_commodity_paper_voucher),
     @SerializedName("Food") FOOD(R.drawable.ic_commodity_food),
     @SerializedName("QR Code Voucher") QR_VOUCHER(R.drawable.ic_commodity_voucher),
+    @SerializedName("Bread") BREAD(R.drawable.ic_commodity_bread),
+    @SerializedName("Agricultural Kit") AGRICULTURAL_KIT(R.drawable.ic_commodity_agricultural_kit),
+    @SerializedName("WASH Kit") WASH_KIT(R.drawable.ic_commodity_wash_kit),
+    @SerializedName("Shelter tool kit") SHELTER_TOOL_KIT(R.drawable.ic_commodity_shelter),
+    @SerializedName("Hygiene kit") HYGIENE_KIT(R.drawable.ic_commodity_hygiene_kit),
+    @SerializedName("Dignity kit") DIGNITY_KIT(R.drawable.ic_commodity_dignity),
     @SerializedName("Mobile Money") MOBILE_MONEY(0)
 }
