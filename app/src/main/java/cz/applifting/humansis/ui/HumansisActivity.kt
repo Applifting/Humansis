01c1eec57667487f159913ebb6d13026fff7505f
Petr Kubeš <petr.kubes@applifting.cz>
2019-12-07 20:47:13 +0100

    Run ssynchronization at proper times

--- app/src/main/java/cz/applifting/humansis/ui/HumansisActivity.kt
+++ app/src/main/java/cz/applifting/humansis/ui/HumansisActivity.kt
@@ -1,22 +1,22 @@
 package cz.applifting.humansis.ui
 
-import android.content.SharedPreferences
+import android.content.*
 import android.graphics.Color
+import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.work.*
 import cz.applifting.humansis.R
 import cz.applifting.humansis.extensions.getDate
 import cz.applifting.humansis.extensions.isWifiConnected
-import cz.applifting.humansis.synchronization.ON_START_SYNC_WORKER
 import cz.applifting.humansis.synchronization.SyncWorker
 import cz.applifting.humansis.synchronization.WHEN_ON_WIFI_SYNC_WORKER
 import cz.applifting.humansis.ui.main.LAST_DOWNLOAD_KEY
 import java.util.*
 import javax.inject.Inject
+
 
 /**
  * Created by Petr Kubes <petr.kubes@applifting.cz> on 11, September, 2019
@@ -26,6 +26,8 @@ class HumansisActivity : AppCompatActivity() {
     @Inject
     lateinit var sp: SharedPreferences
 
+    private val networkChangeReceiver = NetworkChangeReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_humansis)
@@ -33,19 +35,35 @@ class HumansisActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             window.statusBarColor = Color.BLACK
         }
-
-        val navController = findNavController(R.id.nav_host_fragment_base)
+        
         (application as App).appComponent.inject(this)
     }
 
     override fun onResume() {
         super.onResume()
+        enqueSynchronization()
 
+        val filter = IntentFilter()
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+        filter.addAction("android.net.wifi.STATE_CHANGE")
+        registerReceiver(networkChangeReceiver, filter)
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(networkChangeReceiver)
+        super.onDestroy()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        super.onOptionsItemSelected(item)
+        return false
+    }
+
+    private fun enqueSynchronization() {
         val workManager = WorkManager.getInstance(this)
 
         // Try to upload changes as soon as user is online
-        if (!isWifiConnected()) {
-
+        if (lastUploadWasLongTimeAgo()) {
             val whenOnWifiConstraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.UNMETERED)
                 .build()
@@ -55,21 +73,24 @@ class HumansisActivity : AppCompatActivity() {
 
             workManager.enqueueUniqueWork(WHEN_ON_WIFI_SYNC_WORKER, ExistingWorkPolicy.KEEP, syncWhenWifiRequest)
         }
+    }
 
 
+    private fun lastUploadWasLongTimeAgo(): Boolean {
         val lastDownloadDate = sp.getDate(LAST_DOWNLOAD_KEY)
         val calendar = Calendar.getInstance()
         calendar.add(Calendar.HOUR_OF_DAY, -1)
         val dateHourAgo = calendar.time
 
 
-        if (lastDownloadDate != null && lastDownloadDate.before(dateHourAgo)) {
-            workManager.beginUniqueWork(ON_START_SYNC_WORKER, ExistingWorkPolicy.REPLACE, OneTimeWorkRequest.from(SyncWorker::class.java)).enqueue()
-        }
+        return (lastDownloadDate != null && lastDownloadDate.before(dateHourAgo))
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        super.onOptionsItemSelected(item)
-        return false
+    private inner class NetworkChangeReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (isWifiConnected()) {
+                enqueSynchronization()
+            }
+        }
     }
 }
