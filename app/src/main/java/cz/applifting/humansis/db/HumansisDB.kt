2d0959c2c06649e8637868993972572a895d2939
Pavel Haluza <pavel.haluza@applifting.cz>
2020-01-22 17:40:08 +0100

    Remove unused PendingChanges table.

--- app/src/main/java/cz/applifting/humansis/db/HumansisDB.kt
+++ app/src/main/java/cz/applifting/humansis/db/HumansisDB.kt
@@ -11,8 +11,8 @@ import cz.applifting.humansis.model.db.*
  * Created by Petr Kubes <petr.kubes@applifting.cz> on 21, August, 2019
  */
 @Database(
-    entities = [User::class, BeneficiaryLocal::class, ProjectLocal::class, DistributionLocal::class, PendingChangeLocal::class, SyncError::class],
-    version = 15,
+    entities = [User::class, BeneficiaryLocal::class, ProjectLocal::class, DistributionLocal::class, SyncError::class],
+    version = 16,
     exportSchema = false
 )
 @TypeConverters(StringListConverter::class, TargetConverter::class, DateConverter::class, IntListConverter::class, CommodityConverter::class, ReferralTypeConverter::class)
@@ -21,6 +21,5 @@ abstract class HumansisDB : RoomDatabase() {
     abstract fun beneficiariesDao(): BeneficiaryDao
     abstract fun projectsDao(): ProjectsDao
     abstract fun distributionsDao(): DistributionsDao
-    abstract fun pendingChangesDao(): PendingChangesDao
     abstract fun errorsDao(): ErrorDao
 }
